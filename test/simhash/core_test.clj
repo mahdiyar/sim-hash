ba3dec64ce373e9562badf5d915819eff5127ed8
owainlewis <owain@owainlewis.com>
2012-08-29 14:05:36 +0100

    adding a test for the md5 hash algorithm

--- test/simhash/core_test.clj
+++ test/simhash/core_test.clj
@@ -2,6 +2,8 @@
   (:use clojure.test
         simhash.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest md5-test
+  (testing "Hashing strings with MD5"
+    (let [test-string "The quick brown fox jumps over the lazy dog"]
+    (is (= "9e107d9d372bb6826bd81d3542a419d6"
+           (md5 test-string))))))
