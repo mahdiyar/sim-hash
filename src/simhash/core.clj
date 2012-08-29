3f5d5d4ad21b2c39baf7c14c671e440b63209b19
owainlewis <owain@owainlewis.com>
2012-08-29 14:11:40 +0100

    adding note about hamming distance to repo

--- src/simhash/core.clj
+++ src/simhash/core.clj
@@ -3,7 +3,7 @@
 ;; Algorithm utils 
 
 (defn- to-hash
-  "Returns an hashed string"
+  ^{:doc "Returns an hashed string"}
   [alg ^String s]
   (apply str
     (map (partial format "%02x")
@@ -12,6 +12,15 @@
           .reset
           (.update (.getBytes s)))))))
 
+;; Partial functions
+
 (def md5 (partial to-hash "md5"))
 
 (def sha1 (partial to-hash "sha1"))
+
+(defn hamming-distance
+  ^{:doc "Measures the minimum number of substitutions required to
+   change one string into the other, or the number of errors
+   that transformed one string into the other"}
+  [^String s1 ^String s2] )
+                                             
