8fd4c7971973b9dbc9f7cc65ac2474c3b8e71eed
owainlewis <owain@owainlewis.com>
2012-08-29 14:08:01 +0100

    more tests for well known hash results

--- test/simhash/core_test.clj
+++ test/simhash/core_test.clj
@@ -6,4 +6,8 @@
   (testing "Hashing strings with MD5"
     (let [test-string "The quick brown fox jumps over the lazy dog"]
     (is (= "9e107d9d372bb6826bd81d3542a419d6"
-           (md5 test-string))))))
+           (md5 test-string)))
+    (is (= "e4d909c290d0fb1ca068ffaddf22cbd0"
+           (md5 (format "%s." test-string))))
+    (is (= "d41d8cd98f00b204e9800998ecf8427e"
+           (md5 ""))))))
