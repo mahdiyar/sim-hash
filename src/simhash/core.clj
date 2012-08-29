f6ed97ff501338a3bc7849fc619525bb3bd91514
owainlewis <owain@owainlewis.com>
2012-08-29 14:00:53 +0100

    adding basic hashing algorithms

--- src/simhash/core.clj
+++ src/simhash/core.clj
@@ -1,6 +1,17 @@
 (ns simhash.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+;; Algorithm utils 
+
+(defn- to-hash
+  "Returns an hashed string"
+  [alg ^String s]
+  (apply str
+    (map (partial format "%02x")
+      (.digest
+        (doto (java.security.MessageDigest/getInstance alg)
+          .reset
+          (.update (.getBytes s)))))))
+
+(def md5 (partial to-hash "md5"))
+
+(def sha1 (partial to-hash "sha1"))
